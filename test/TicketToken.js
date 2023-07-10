a77429c509bfe2bbbf9e308137ba4b5882e708a9
ThomasHeim11 <wearedreamin@gmail.com>
2023-07-10 10:19:22 +0200

    update unit test✅

--- test/TicketToken.js
+++ test/TicketToken.js
@@ -1,18 +1,27 @@
 const { expect } = require("chai");
 
+const NAME = "TicketToken";
+const SYMBOL = "TT";
+
 describe("TicketToken", () => {
+  let TicketToken;
+  let deployer, buyer;
+
   beforeEach(async () => {
+    [deployer, buyer] = await ethers.getSigners()
+
+
     const TicketToken = await ethers.getContractFactory("TicketToken");
-    ticketToken = await TicketToken.deploy("TicketToken", "TT");
+    ticketToken = await TicketToken.deploy(NAME, SYMBOL);
   });
 
   describe("Deployment", () => {
     it("Sets the name", async () => {
-      expect(await ticketToken.name()).to.equal("TicketToken");
+      expect(await ticketToken.name()).to.equal(NAME);
     });
 
     it("Sets the symbol", async () => {
-      expect(await ticketToken.symbol()).to.equal("TT");
+      expect(await ticketToken.symbol()).to.equal(SYMBOL);
     });
   });
 });
