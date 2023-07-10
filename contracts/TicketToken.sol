ae947912660ff52e2e08a9ba065670ba5f6870f3
ThomasHeim11 <wearedreamin@gmail.com>
2023-07-10 09:39:53 +0200

    update smart contract✅

--- contracts/TicketToken.sol
+++ contracts/TicketToken.sol
@@ -4,5 +4,8 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TicketToken is ERC721 {
-    
+
+    constructor(string _name, string _symbol) {
+
+    }
 }
