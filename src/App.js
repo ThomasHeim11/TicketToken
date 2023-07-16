853d2a4aaf2b43f81d82919e95fc4e518c6c4e3e
ThomasHeim11 <wearedreamin@gmail.com>
2023-07-16 17:42:47 +0200

    update ui✅

--- src/App.js
+++ src/App.js
@@ -1,68 +1,74 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 
 // Components
-import Navigation from './components/Navigation'
-import Sort from './components/Sort'
-import Card from './components/Card'
-import SeatChart from './components/SeatChart'
+import Navigation from './components/Navigation';
+import Sort from './components/Sort';
+import Card from './components/Card';
+import SeatChart from './components/SeatChart';
 
 // ABIs
-import TicketToken from './abis/TicketToken.json'
+import TicketToken from './abis/TicketToken.json';
 
 // Config
-import config from './config.json'
+import config from './config.json';
 
 function App() {
-  const [provider, setProvider] = useState(null)
-  const [account, setAccount] = useState(null)
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
 
-  const [ticketToken, setTicketToken] = useState(null)
-  const [occasions, setOccasions] = useState([])
+  const [ticketToken, setTicketToken] = useState(null);
+  const [occasions, setOccasions] = useState([]);
 
-  const [occasion, setOccasion] = useState({})
-  const [toggle, setToggle] = useState(false)
+  const [occasion, setOccasion] = useState({});
+  const [toggle, setToggle] = useState(false);
 
   const loadBlockchainData = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    setProvider(provider)
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
 
-    const network = await provider.getNetwork()
-    const ticketToken = new ethers.Contract(config[network.chainId].TicketToken.address, TicketToken, provider)
-    setTicketToken(ticketToken)
+    const network = await provider.getNetwork();
+    const ticketToken = new ethers.Contract(
+      config[network.chainId].TicketToken.address,
+      TicketToken,
+      provider
+    );
+    setTicketToken(ticketToken);
 
-    const totalOccasions = await ticketToken.totalOccasions()
-    const occasions = []
+    const totalOccasions = await ticketToken.totalOccasions();
+    const occasions = [];
 
     for (var i = 1; i <= totalOccasions; i++) {
-      const occasion = await ticketToken.getOccasion(i)
-      occasions.push(occasion)
+      const occasion = await ticketToken.getOccasion(i);
+      occasions.push(occasion);
     }
 
-    setOccasions(occasions)
+    setOccasions(occasions);
 
     window.ethereum.on('accountsChanged', async () => {
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-      const account = ethers.utils.getAddress(accounts[0])
-      setAccount(account)
-    })
-  }
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = ethers.utils.getAddress(accounts[0]);
+      setAccount(account);
+    });
+  };
 
   useEffect(() => {
-    loadBlockchainData()
-  }, [])
+    loadBlockchainData();
+  }, []);
 
   return (
-    <div>
+    <div className="dark-bg">
       <header>
         <Navigation account={account} setAccount={setAccount} />
 
-        <h2 className="header__title"><strong>Event</strong> Tickets</h2>
+        <h2 className="header__title">
+          <strong>Event</strong> Tickets
+        </h2>
       </header>
 
       <Sort />
 
-      <div className='cards'>
+      <div className="cards">
         {occasions.map((occasion, index) => (
           <Card
             occasion={occasion}
