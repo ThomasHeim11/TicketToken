009d85718a443c7d3399eb6cb047228b04d59ff1
ThomasHeim11 <wearedreamin@gmail.com>
2023-07-16 16:12:56 +0200

    update ui✅

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,38 +1,53 @@
-import { ethers } from 'ethers'
+import { ethers } from "ethers";
 
 const Navigation = ({ account, setAccount }) => {
   const connectHandler = async () => {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    const account = ethers.utils.getAddress(accounts[0])
-    setAccount(account)
-  }
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
+  };
 
   return (
     <nav>
-      <div className='nav__brand'>
+      <div className="nav__brand">
         <h1>TicketToken</h1>
 
-        <input className='nav__search' type="text" placeholder='Find millions of experiences' />
+        <input
+          className="nav__search"
+          type="text"
+          placeholder="Find millions of experiences"
+        />
 
-        <ul className='nav__links'>
-          <li><a href="/">Concerts</a></li>
-          <li><a href="/">Sports</a></li>
-          <li><a href="/">Arts & Theater</a></li>
-          <li><a href="/">More</a></li>
+        <ul className="nav__links">
+          <li>
+            <a href="/">Concerts</a>
+          </li>
+          <li>
+            <a href="/">Sports</a>
+          </li>
+          <li>
+            <a href="/">Arts & Theater</a>
+          </li>
+          <li>
+            <a href="/">More</a>
+          </li>
         </ul>
       </div>
 
       {account ? (
         <button
           type="button"
-          className='nav__connect'
+          className="nav__connect rounded-button"
+          onClick={connectHandler}
         >
-          {account.slice(0, 6) + '...' + account.slice(38, 42)}
+          {account.slice(0, 6) + "..." + account.slice(38, 42)}
         </button>
       ) : (
         <button
           type="button"
-          className='nav__connect'
+          className="nav__connect rounded-button"
           onClick={connectHandler}
         >
           Connect
@@ -40,6 +55,6 @@ const Navigation = ({ account, setAccount }) => {
       )}
     </nav>
   );
-}
+};
 
 export default Navigation;
